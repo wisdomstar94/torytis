1b45c6d526d687eb47106ef2722a0b65d0b6ad21
wisdomstar94 <wisdomstar94@gmail.com>
2023-10-02 08:53:35 +0900

    v0.0.15 : tailwind.config 파일을 js 에서 ts 로 변경.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss';
+
+export default {
   content: [
     './src/**/*.tsx', './src/**/*.scss', './src/**/*.ts',
   ],
@@ -7,5 +8,5 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+} satisfies Config
 
