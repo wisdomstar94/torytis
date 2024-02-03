ae04fc47dd2cdb284d1a212688846118553f26f1
wisdomstar94 <wisdomstar94@gmail.com>
2024-02-03 22:00:44 +0900

    [ v1.1.0-test.1 ] "/cagegory", "/cagegory/:param1/:param2" 페이지 작업.

--- src/sub_commands/c_dev/routes/category.rs
+++ src/sub_commands/c_dev/routes/category.rs
@@ -4,13 +4,62 @@ use crate::{common::get_skin_html_content, structs::{replacer::{ApplyGuestBookOp
 
 pub fn routes() -> Router {
     Router::new()
+        .route("/", get(root_route))
         .route("/:category_name", get(category_index_route))
+        .route("/:category_name/:sub_category_name", get(category_sub_category_index_route))
         // .route("/style.css", get(style_css_route))
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct RootPageQueryPayload {
     page: Option<u32>,
+}
+
+async fn root_route(req: Request) -> Response {
+    let mut query_option: Option<RootPageQueryPayload> = None;
+    if let Some(query_str) = req.uri().query() {
+        if let Ok(payload) = serde_qs::from_str::<RootPageQueryPayload>(query_str) {
+          query_option = Some(payload);
+        } else {
+          println!("query_str : {:?}", query_str);
+        }
+    }
+
+    let size = 8;
+    let page = if let Some(q) = query_option {
+        q.page.unwrap_or_else(|| 1)
+    } else {
+        1
+    };
+
+    let skin_html_content = get_skin_html_content();
+    let replacer = Replacer::new(&skin_html_content);
+    replacer.apply_index_page(ApplyIndexPageOptions {
+        base_url: format!(r#"/category"#),
+        body_id: String::from("tt-body-category"),
+        apply_index_list_option: ApplyIndexListOptions {
+            is_hide: false,
+            post_select_option: PostSelectOption {
+                page: Some(page),
+                size: Some(size),
+                post_type: None,
+                category_name: None,
+                sub_category_name: None,
+            },
+        },
+        apply_guest_book_option: ApplyGuestBookOptions {
+            is_hide: true,
+        },
+        apply_tag_list_option: ApplyTagListOptions {
+            is_hide: true,
+        },
+    });
+
+    return Response::builder()
+      .status(StatusCode::OK)
+      .header("Content-Type", "text/html")
+      .body(Body::from(replacer.get_html()))
+      .unwrap();
 }
 
 async fn category_index_route(Path(category_name): Path<String>, req: Request) -> Response {
@@ -30,7 +79,6 @@ async fn category_index_route(Path(category_name): Path<String>, req: Request) -
         1
     };
 
-    let config = TorytisDevConfig::new();
     let skin_html_content = get_skin_html_content();
     let replacer = Replacer::new(&skin_html_content);
     replacer.apply_index_page(ApplyIndexPageOptions {
@@ -53,19 +101,61 @@ async fn category_index_route(Path(category_name): Path<String>, req: Request) -
             is_hide: true,
         },
     });
-    // skin_html_content = replace_s_search(&skin_html_content, "");
-    // skin_html_content = replace_common(&config, &skin_html_content);
-    // skin_html_content = replace_home_display(&config, &skin_html_content);
-    // skin_html_content = replace_var_page_title(&skin_html_content, config.get_blog_title());
-    // skin_html_content = replace_var_body_id(&skin_html_content, "tt-body-index");
 
-    // let skin_html_content_str = skin_html_content.as_str();
     return Response::builder()
       .status(StatusCode::OK)
       .header("Content-Type", "text/html")
       .body(Body::from(replacer.get_html()))
       .unwrap();
 }
+
+async fn category_sub_category_index_route(Path((category_name, sub_category_name)): Path<(String, String)>, req: Request) -> Response {
+    let mut query_option: Option<RootPageQueryPayload> = None;
+    if let Some(query_str) = req.uri().query() {
+        if let Ok(payload) = serde_qs::from_str::<RootPageQueryPayload>(query_str) {
+          query_option = Some(payload);
+        } else {
+          println!("query_str : {:?}", query_str);
+        }
+    }
+
+    let size = 8;
+    let page = if let Some(q) = query_option {
+        q.page.unwrap_or_else(|| 1)
+    } else {
+        1
+    };
+
+    let skin_html_content = get_skin_html_content();
+    let replacer = Replacer::new(&skin_html_content);
+    replacer.apply_index_page(ApplyIndexPageOptions {
+        base_url: format!(r#"/category/{}/{}"#, category_name, sub_category_name),
+        body_id: String::from("tt-body-category"),
+        apply_index_list_option: ApplyIndexListOptions {
+            is_hide: false,
+            post_select_option: PostSelectOption {
+                page: Some(page),
+                size: Some(size),
+                post_type: None,
+                category_name: Some(category_name.clone()),
+                sub_category_name: Some(sub_category_name.clone()),
+            },
+        },
+        apply_guest_book_option: ApplyGuestBookOptions {
+            is_hide: true,
+        },
+        apply_tag_list_option: ApplyTagListOptions {
+            is_hide: true,
+        },
+    });
+
+    return Response::builder()
+      .status(StatusCode::OK)
+      .header("Content-Type", "text/html")
+      .body(Body::from(replacer.get_html()))
+      .unwrap();
+}
+
 
 // async fn style_css_route() -> Response {
 //     let content = get_style_css_content();
