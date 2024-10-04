fbadab7d75951067d48ad4f50a432a9da17c2733
wisdomstar94 <wisdomstar94@gmail.com>
2024-10-04 12:54:47 +0900

    binary file download url 수정.

--- src/packages/torytis/npm/getBinary.js
+++ src/packages/torytis/npm/getBinary.js
@@ -1,32 +1,38 @@
-const { Binary } = require('binary-install');
-const os = require('os');
-const path = require('path');
+const { Binary } = require("binary-install");
+const os = require("os");
+const path = require("path");
 
 function getPlatform() {
   const type = os.type();
   const arch = os.arch();
 
   const platformInfo = {
-    arch: '',
-    type: '',
+    arch: "",
+    type: "",
   };
 
-  switch(type) {
-    case 'Windows_NT': {
-      platformInfo.type = 'pc-windows-msvc';
-    } break;
-    case 'Darwin': {
-      platformInfo.type = 'apple-darwin';
-    } break;
-    case 'Linux': {
-      platformInfo.type = 'linux-gnu';
-    } break;
+  switch (type) {
+    case "Windows_NT":
+      {
+        platformInfo.type = "pc-windows-msvc";
+      }
+      break;
+    case "Darwin":
+      {
+        platformInfo.type = "apple-darwin";
+      }
+      break;
+    case "Linux":
+      {
+        platformInfo.type = "linux-gnu";
+      }
+      break;
   }
 
-  if (['arm', 'arm64'].includes(arch)) {
-    platformInfo.arch = 'aarch64';
+  if (["arm", "arm64"].includes(arch)) {
+    platformInfo.arch = "aarch64";
   } else {
-    platformInfo.arch = 'x86_64';
+    platformInfo.arch = "x86_64";
   }
 
   return platformInfo;
@@ -34,10 +40,11 @@ function getPlatform() {
 
 function getBinary() {
   const platform = getPlatform();
-  const version = require('../package.json').version;
-  const url = `https://github.com/wisdomstar94/torytis/releases/download/v${version}/torytis-${platform.arch}-${platform.type}.tar.gz`;
-  const name = 'torytis';
-  const installDirectory = path.join(__dirname, '..', 'bin');
+  const version = require("../package.json").version;
+  // const url = `https://github.com/wisdomstar94/torytis/releases/download/v${version}/torytis-${platform.arch}-${platform.type}.tar.gz`;
+  const url = `https://github.com/wisdomstar94/torytis/releases/download/torytis-v${version}/torytis-${platform.arch}-${platform.type}.tar.gz`;
+  const name = "torytis";
+  const installDirectory = path.join(__dirname, "..", "bin");
   return new Binary(name, url, {
     installDirectory,
   });
