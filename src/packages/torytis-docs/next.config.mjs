d392ffd2a90ce14982604a6dbd0d312671fe1478
wisdomstar94 <wisdomstar94@gmail.com>
2024-10-05 23:32:55 +0900

    [torytis-docs] codeblock 에 highlight 적용.

--- src/packages/torytis-docs/next.config.mjs
+++ src/packages/torytis-docs/next.config.mjs
@@ -1,14 +1,30 @@
 import createMDX from "@next/mdx";
+import rehypePrettyCode from "rehype-pretty-code";
+
+/** @type {import('rehype-pretty-code').Options} */
+const options = {
+  // See Options section below.
+  theme: "rose-pine-dawn",
+  // theme: "snazzy-light",
+  keepBackground: true,
+};
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   basePath: "/torytis",
   output: "export",
+  experimental: {
+    mdxRs: false,
+  },
 };
 
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [[rehypePrettyCode, options]],
+  },
 });
 
 export default withMDX(nextConfig);
