9329680e547749ebbe74eafb2513aa03dd3ea101
wisdomstar94 <wisdomstar94@gmail.com>
2024-10-12 10:52:49 +0900

    소스코드 변경시 브라우저에서 새로고침까지 되도록 구현.

--- src/packages/torytis/static/socket-dispose.js
+++ src/packages/torytis/static/socket-dispose.js
@@ -1,6 +1,11 @@
 window.addEventListener("load", () => {
   console.log("loade됨", io);
 
-  const socketUrl = ``;
+  const SOCKET_PORT = 3008;
+  const socketUrl = `localhost:${SOCKET_PORT}`;
   const socket = io(`ws://${socketUrl.replace("http://", "").replace("https://", "").replace("ws://", "")}`);
+
+  socket.on("full-reload", () => {
+    location.reload();
+  });
 });
